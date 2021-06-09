45a01e2f50983f25d029a5177526f0bf49db9ac2
Ryan <ryanpaynt@uncc.edu>
2021-06-09 16:15:37 -0400

    Writing test Employee test file

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -7,29 +7,43 @@ test("Can instantiate Employee instance", () => {
 });
 
 test("Can set name via constructor arguments", () => {
-
+    const name = "Ryan";
+    const employee = new Emplooyee(name);
+    expect(employee.name).toBe(name);
 });
 
 test("Can set id via constructor argument", () => {
-
+    const testVal = 1;
+    const employee = new Employee("Ryan", testVal);
+    expect(employee.id).toBe(testVal);
 });
 
 test("Can set email via constructor argument", () => {
-
+    const emailVal = 'test@email.com';
+    const employee = new Employee("Ryan", 1, emailVal);
+    expect(employee.email).toBe(emailVal);
 });
 
 test("Can get name via getName()", () => {
-
+    const name = "Ryan";
+    const employee = new Emplooyee(name);
+    expect(employee.getName()).toEqual(name);
 });
 
 test("Can get id via getId()", () => {
-
+    const testVal = 1;
+    const employee = new Employee("Ryan", testVal);
+    expect(employee.getId()).toEqual(testVal);
 });
 
 test("Can get email via getEmail()", () => {
-
+    const emailVal = 'test@email.com';
+    const employee = new Employee("Ryan", 1, emailVal);
+    expect(employee.getEmail()).toEqual(emailVal);
 });
 
 test("getRole() should return \"Employee\"", () => {
-
+    const roleVal = "Employee";
+    const employee = new Employee("Ryan", 1, "test@email.com");
+    expect(e.getRole()).toEqual(roleValue);
 });
