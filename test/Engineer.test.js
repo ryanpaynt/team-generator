8eab695c7fcc0a99056e8f61e7afd8a54b6fe006
Ryan <ryanpaynt@uncc.edu>
2021-06-09 16:49:19 -0400

    Writing Engineer test file

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -5,13 +5,17 @@ const Engineer = require("../lib/Engineer");
 test("Can set GitHUb account via constructor", () => {
     const ghLink = 'www.link@gitHub.com';
     const eng = Engineer("Ryan", 1, "test@email.com", ghLink);
-    expect(eng.github()).toBe(ghLink);
+    expect(eng.gitHub()).toBe(ghLink);
 });
 
 test("getRole() should return \"Engineer\"", () => {
-
+    const role = "Engineer";
+    const eng = Engineer("Ryan", 1, "test@email.com", 'www.link@gitHub.com');
+    expect(eng.getRole()).toBe(role);
 });
 
 test("Can get GitHub username via getGithub()", () => {
-
+    const usrVal = 'www.link@gitHub.com';
+    const eng = Engineer("Ryan", 1, "test@email.com", usrVal);
+    expect(eng.getGitHub()).toBe(usrVal);
 });
